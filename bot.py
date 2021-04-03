c20cb8b52b0073ed1c8894d844767109250b7d88
mmonkey <mmonkey@gmail.com>
2021-04-03 00:41:43 -0500

    added vaccinespotter.org api support

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
-import argparse
 import discord
 import json
 import os
 import pytz
 import requests
-import sys
 from datetime import datetime
 from discord.ext import tasks
 from dotenv import load_dotenv
+from geopy import distance
 
 # Load environment variables
 load_dotenv()
@@ -26,16 +25,23 @@ class CovidVaccineBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.locations = []
-        self.availability = {}
-        self.newly_available = []
         self.message = ''
+        self.hyvee_locations = []
+        self.hyvee_availability = {}
+        self.newly_available_hyvee_appointments = []
+
+        self.spotter_cache = {}
+        self.spotter_locations = []
+        self.spotter_availability = {}
+        self.newly_available_spotter_appointments = []
+
         self.check_for_vaccine_availability_task.start()
 
     async def on_ready(self):
         print(f'Logged in as {self.user.name} ({self.user.id})')
 
-    def get_vaccine_availability(self, latitude, longitude, radius):
+    # Hy-Vee Pharmacies
+    def get_hyvee_vaccine_availability(self, latitude, longitude, radius):
         url = 'https://www.hy-vee.com/my-pharmacy/api/graphql'
         query = '''
             query SearchPharmaciesNearPointWithCovidVaccineAvailability($latitude: Float!, $longitude: Float!, $radius: Int! = 10) {
@@ -76,44 +82,95 @@ class CovidVaccineBot(discord.Client):
 
         if response.status_code == 200:
             json_data = json.loads(response.text)
-            self.locations = list(map(lambda location: location['location'], json_data['data']['searchPharmaciesNearPoint']))
+            self.hyvee_locations = list(map(lambda location: location['location'], json_data['data']['searchPharmaciesNearPoint']))
 
         else:
             print(f'Error getting vaccine availability: {response.text}')
-            self.locations = []
+            self.hyvee_locations = []
 
-    def get_newly_available_locations(self, is_test=False):
-        self.newly_available = []
+    def get_newly_available_hyvee_locations(self, is_test=False):
+        self.newly_available_hyvee_appointments = []
 
-        for location in self.locations:
-            prev_avail = self.availability[location['locationId']] if location['locationId'] in self.availability else False
+        for location in self.hyvee_locations:
+            prev_avail = self.hyvee_availability[location['locationId']] if location['locationId'] in self.hyvee_availability else False
 
             if location['isCovidVaccineAvailable'] and not prev_avail:
-                self.newly_available.append(location)
+                self.newly_available_hyvee_appointments.append(location)
 
-            if not len(self.newly_available) and is_test:
-                self.newly_available.append(location)
+            if not len(self.newly_available_hyvee_appointments) and is_test:
+                self.newly_available_hyvee_appointments.append(location)
 
-            self.availability[location['locationId']] = location['isCovidVaccineAvailable']
+            self.hyvee_availability[location['locationId']] = location['isCovidVaccineAvailable']
 
-    def message_start(self):
-        self.message = '@everyone :syringe: appointments available!'
-
-    def message_location(self, location):
+    def message_hyvee_location(self, location):
         name = location['nickname'] if location['nickname'] else location['name']
         address = location['address']['line1']
         city = location['address']['city']
         state = location['address']['state']
         zipcode = location['address']['zip']
-        self.message += f'\n\n{name}\n{address}\n{city}, {state} {zipcode}'
+        url = 'https://www.hy-vee.com/my-pharmacy/covid-vaccine-consent'
+        self.message += f'\n\n{name}\n{address}\n{city}, {state} {zipcode}\n{url}'
+
+    # Other Pharmacies - vaccinespotter.org API
+    def get_spotter_api_vaccine_availability(self, latitude, longitude, radius, states):
+        locations = []
+        for state in states:
+            if state not in self.spotter_cache:
+                response = requests.get(f'https://www.vaccinespotter.org/api/v0/states/{state}.json')
+
+                if response.status_code == 200:
+                    json_data = json.loads(response.text)
+                    self.spotter_cache[state] = list(
+                        filter(lambda loc: loc['properties']['provider'] != 'hyvee', json_data['features'])
+                    )
+                else:
+                    self.spotter_cache[state] = []
+
+            locations.extend(self.spotter_cache[state])
+
+        self.spotter_locations = []
+        search_coordinates = (latitude, longitude)
+        for location in locations:
+            location_coordinates = (location['geometry']['coordinates'][1], location['geometry']['coordinates'][0])
+            if distance.distance(search_coordinates, location_coordinates).miles <= radius:
+                self.spotter_locations.append(location)
+
+    def get_newly_available_spotter_locations(self, is_test=False):
+        self.newly_available_spotter_appointments = []
+
+        for location in self.spotter_locations:
+            prev_avail = self.spotter_availability[location['properties']['id']] if location['properties']['id'] in self.spotter_availability else False
+
+            if location['properties']['appointments_available'] and not prev_avail:
+                self.newly_available_spotter_appointments.append(location)
+
+            if not len(self.newly_available_spotter_appointments) and is_test:
+                self.newly_available_spotter_appointments.append(location)
+
+            self.spotter_availability[location['properties']['id']] = location['properties']['appointments_available']
+
+    def message_spotter_location(self, location):
+        name = '{name} {provider}'.format(
+            name=location['properties']['name'],
+            provider=location['properties']['provider_brand_name']
+        )
+        address = location['properties']['address']
+        city = location['properties']['city']
+        state = location['properties']['state']
+        zipcode = location['properties']['postal_code']
+        url = location['properties']['url']
+        self.message += f'\n\n{name}\n{address}\n{city}, {state} {zipcode}\n{url}'
+
+    # Messaging header/footer
+    def message_header(self):
+        self.message = '@everyone :syringe: appointments available!'
 
     def message_footer(self):
         cst = pytz.timezone('US/Central')
         dt = datetime.now(cst)
         timestamp = dt.strftime('%b %d, %Y at %I:%M:%S %p')
 
-        self.message += '\n\nRegister Here: https://www.hy-vee.com/my-pharmacy/covid-vaccine-consent'
-        self.message += f'\nPosted {timestamp} CST'
+        self.message += f'\n\nPosted {timestamp} CST'
 
     @tasks.loop(seconds=30)
     async def check_for_vaccine_availability_task(self):
@@ -122,17 +179,33 @@ class CovidVaccineBot(discord.Client):
 
             if enabled:
                 if config['latitude'] and config['longitude'] and config['radius']:
-                    test = config['test'] if 'test' in config else False
-                    self.get_vaccine_availability(config['latitude'], config['longitude'], config['radius'])
-                    self.get_newly_available_locations(test)
+                    is_test = config['test'] if 'test' in config else False
+                    self.get_hyvee_vaccine_availability(config['latitude'], config['longitude'], config['radius'])
+                    self.get_newly_available_hyvee_locations(is_test)
+
+                    # clear the spotter cache
+                    self.spotter_cache = {}
+                    states = config['states'] if 'states' in config else ['NE']
+                    self.get_spotter_api_vaccine_availability(
+                        config['latitude'],
+                        config['longitude'],
+                        config['radius'],
+                        states)
+                    self.get_newly_available_spotter_locations(is_test)
                 else:
                     print('invalid config: missing geolocation data')
 
-                if len(self.newly_available):
-                    print(f'{len(self.newly_available)} newly available location(s) found')
-                    self.message_start()
-                    for location in self.newly_available:
-                        self.message_location(location)
+                if len(self.newly_available_hyvee_appointments) or len(self.newly_available_spotter_appointments):
+                    print(f'{len(self.newly_available_hyvee_appointments)} newly available hy-vee location(s) found')
+                    print(f'{len(self.newly_available_spotter_appointments)} newly available spotter location(s) found')
+
+                    self.message_header()
+                    for location in self.newly_available_hyvee_appointments:
+                        self.message_hyvee_location(location)
+
+                    for location in self.newly_available_spotter_appointments:
+                        self.message_spotter_location(location)
+
                     self.message_footer()
 
                     if config['channel']:
